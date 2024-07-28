643f88bb2b57ca6ef4f1e03fe75bf4dc07934f86
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-07-28 09:24:24 -0400

    debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Event } from "./app/types";
 import { FiltersMenu } from "./app/components/FiltersMenu";
 import { SearchBarInput } from "./app/components/SearchBarInput";
 import { useSelector } from "react-redux";
 import { RootState } from "./app/store";
+import debounce from "lodash.debounce";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
@@ -15,18 +16,35 @@ function App() {
   const searchValue = useSelector(
     (state: RootState) => state.searchbarValue.searchbarValue
   );
+  const filters = useSelector(
+    (state: RootState) => state.filters.queuedUpFilters
+  );
+
+  const getEvents = useCallback(
+    async (searchValue: string | null, filters: any) => {
+      let query = supabase.from("Events").select();
+
+      if (searchValue) {
+        query = query.ilike("title", `%${searchValue}%`);
+      }
+
+      Object.entries(filters).forEach(([key, value]) => {
+        if (value && value !== "None") {
+          query = query.or(`tags.ilike.%${value}%, title.ilike.%${value}%`);
+        }
+      });
+
+      const { data } = await query;
+      setEvents(data);
+    },
+    []
+  );
+
+  const debouncedGetEvents = useCallback(debounce(getEvents, 300), [getEvents]); // using useCallback here to ensure that getEvents() is only re-created when its dependencies change
 
   useEffect(() => {
-    getEvents(searchValue);
-  }, [searchValue]);
-
-  async function getEvents(searchValue: string | null, filters?: any[]) {
-    const { data } = await supabase
-      .from("Events")
-      .select()
-      .ilike("title", `%${searchValue}%`);
-    setEvents(data);
-  }
+    debouncedGetEvents(searchValue, filters);
+  }, [searchValue, filters, debouncedGetEvents]);
 
   return (
     <>
