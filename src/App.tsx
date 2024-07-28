31f95ec6e4111b1f2a8eb1dc6e8990eb7de2c232
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-07-27 23:57:18 -0400

    some functional search bar stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Event } from "./app/types";
 import { FiltersMenu } from "./app/components/FiltersMenu";
 import { SearchBarInput } from "./app/components/SearchBarInput";
+import { useSelector } from "react-redux";
+import { RootState } from "./app/store";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
@@ -10,13 +12,19 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function App() {
   const [events, setEvents] = useState<Event[] | null>([]);
+  const searchValue = useSelector(
+    (state: RootState) => state.searchbarValue.searchbarValue
+  );
 
   useEffect(() => {
-    getEvents();
-  }, []);
+    getEvents(searchValue);
+  }, [searchValue]);
 
-  async function getEvents() {
-    const { data } = await supabase.from("Events").select();
+  async function getEvents(searchValue: string | null, filters?: any[]) {
+    const { data } = await supabase
+      .from("Events")
+      .select()
+      .ilike("title", `%${searchValue}%`);
     setEvents(data);
   }
 
