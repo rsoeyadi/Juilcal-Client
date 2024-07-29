b81914c5fcc02d7db9419a71488d87f5077e8cc3
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-07-29 10:07:34 -0400

    setting up bookmark

--- src/app/components/cards/EventCard.tsx
+++ src/app/components/cards/EventCard.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { AppDispatch } from "../../store";
+
 type EventCardProps = {
   title: string;
   date: string;
@@ -5,21 +9,26 @@ type EventCardProps = {
   link: string;
 };
 
+type BookmarkButtonProps = {
+  dispatch: AppDispatch;
+};
 
-const BookmarkButton = () => {
-    const handleClick = () => {
-        
-    }
+const BookmarkButton = ({ dispatch }: BookmarkButtonProps) => {
+  const [bookmarked, setBookmarked] = useState<boolean>(false);
+  const handleClick = () => {
+    setBookmarked(!bookmarked);
+  };
   return <button></button>;
 };
 export const EventCard = ({ title, date, venue, link }: EventCardProps) => {
+  const dispatch = useAppDispatch();
   return (
     <>
       <h1>{title}</h1>
       <p>{date}</p>
       <p> {venue}</p>
       <p> {link}</p>
-      <BookmarkButton />
+      <BookmarkButton dispatch={dispatch} />
     </>
   );
 };
